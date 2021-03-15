4474b034fae01bff4e99363a70927c1ab2f63416
shkim4738 <shkim4738@gmail.com>
2021-03-15 22:02:25 +0900

    백준 - 탐색 - 배열 돌리기 4 (진행중)

--- actual-practice/05_search/q020_01_rotate-array-4.py
+++ actual-practice/05_search/q020_01_rotate-array-4.py
@@ -2,3 +2,13 @@
 # url : https://www.acmicpc.net/problem/17406
 
 # 직접 풀이 (1) - 
+
+N, M, K = map(int, input().split())
+
+A = [list(map(int, input().split())) for _ in range(N)]
+O = [list(map(int, input().split())) for _ in range(K)]
+
+for a in A:
+    print(a)
+
+print(O)
